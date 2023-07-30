823a1ee7e144e04b2816acc1c6cd932cfbbdb484
Erick Hilda <erickh.andreas@gmail.com>
2023-07-30 12:04:44 +0700

    feat: remove dark mode and add configurator page

---
+++ src/app/configurator/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Chair } from "@/components/chair";
+import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
+function ConfiguratorPage() {
+  return (
+    <div className="h-full">
+      <Canvas shadows camera={{ position: [0, 2, 4], fov: 45 }}>
+        <ambientLight intensity={0.7} />
+        <spotLight
+          intensity={0.5}
+          angle={0.1}
+          penumbra={1}
+          position={[10, 15, 10]}
+          castShadow
+        />
+
+        <Chair position={[0, -0.78, 0]} scale={1.5} />
+
+        <Environment preset="city" />
+        <ContactShadows
+          position={[0, -0.8, 0]}
+          opacity={0.25}
+          scale={10}
+          blur={1.5}
+          far={0.8}
+        />
+        <OrbitControls
+          minPolarAngle={Math.PI / 3}
+          maxPolarAngle={Math.PI / 2}
+          enableZoom={false}
+          enablePan={false}
+          enableDamping
+          dampingFactor={0.1}
+        />
+      </Canvas>
+    </div>
+  );
+}
+
+export default ConfiguratorPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import clsx from 'clsx'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,9 +15,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={clsx('h-screen', inter.className)}>{children}</body>
     </html>
   )
 }
